e19b167468cfc11014b2f01687f880b3ed1ce0be
FaizanSipra <faizansipra4545@gmail.com>
2026-09-25 04:50:33 +0500

    07-ui-project little fix done

--- 07-ui-project/src/components/Section1/HeroText.jsx
+++ 07-ui-project/src/components/Section1/HeroText.jsx
@@ -4,7 +4,7 @@ const HeroText = () => {
     return (
         <div className='p-6'>
             <h3 className='text-6xl mb-7 font-bold'>Prospective<br /><span>customer</span><br />segmentation</h3>
-            <p className='text-xl font-medium text-gray-600'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem totam pariatur impedit perferendis sequi vero sit cum consequatur maiores voluptatem?</p>
+            <p className='text-xl font-medium text-gray-600 w-[90%]'>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Dolorem totam pariatur impedit perferendis sequi vero sit cum consequatur maiores voluptatem?</p>
         </div>
     )
 }
